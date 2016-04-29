47d83d5a556850b6ef37818f239bf4def796e336
TedMANNERs <mannersofted@gmail.com>
2016-04-29 16:51:11 +0200

    Added file path to track table

--- doc/database.sql
+++ doc/database.sql
@@ -4,7 +4,8 @@ CREATE EXTENSION "uuid-ossp";
 /* TABLES */
 CREATE TABLE track (
   "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  "name" TEXT NOT NULL
+  "name" TEXT NOT NULL,
+  "filepath" TEXT NOT NULL
 );
 
 CREATE TABLE tag (
